9659d0942858ffa42ee8f4f878ade3b951110fef
sandy1301 <megalaanandhan59@gmail.com>
2025-12-17 18:21:35 +0530

    first commit

---
+++ function.js
@@ -0,0 +1,21 @@
+let a=3,b=6;
+
+function op(a1,b1){
+    console.log(a1+b1-b1*b1/a1)
+}
+
+op(a,b);
+
+function name(fname,lname){
+    console.log("hii " + fname +" "+ lname);
+
+}
+name("sandhiya","santhosh");
+
+let k=2,l=4;
+function number(odd,even){
+     if(number%2!=0){
+     console.log("the number is" + odd);
+
+}}
+number(2,9);
